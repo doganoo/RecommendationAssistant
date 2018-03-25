9a27a24889eff7c4fd97e8650788dac9f21a70b3
Dogan Ucar <dogan@dogan-ucar.de>
2018-03-25 15:17:40 +0200

    master sync

--- lib/ContentReader/RTFReader.php
+++ lib/ContentReader/RTFReader.php
@@ -1,9 +1,22 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: dogano
- * Date: 05.12.17
- * Time: 13:45
+ * @copyright Copyright (c) 2017, Dogan Ucar (dogan@dogan-ucar.de)
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 namespace OCA\RecommendationAssistant\ContentReader;
